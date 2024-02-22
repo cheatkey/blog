62dc89b7aa3aa1b6aacbeec63ff6d96fc633af4e
권신학 <cheatkey.dev@gmail.com>
2024-02-23 05:36:19 +0900

    메인 페이지 UI 레이아웃 제작

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,88 @@
 import Image from "next/image";
+import PostCard from "@/app/components/PostCard";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <div className="flex justify-center bg-gray-900 py-20">
+      <main className="max-w-[1200px] w-full p-4 flex flex-col gap-24 py-12">
+        <section className="max-w-[1200px] w-full px-4 py-40">
+          <p className="text-2xl text-gray-50">개발 블로그</p>
+          <span className="text-gray-200">블로그 소개 내용 입력</span>
+          <p className="text-gray-300">github, email 아이콘 링크</p>
+        </section>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <section className="flex flex-col gap-2 max-w-[1200px] w-full px-4">
+          <h2 className="text-4xl font-bold text-gray-100">
+            시리즈로 작성된 글
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          <div className="w-full h-48 bg-gray-600 rounded-lg p-6">
+            zustand 마스터리
+          </div>
+          <div className="w-full h-48 bg-gray-600 rounded-lg p-6">
+            Notion 생산성 with notion api, chatgpt
+          </div>
+          <div className="w-full h-48 bg-gray-600 rounded-lg p-6">
+            프론트엔드 디버깅 101
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <p className="text-gray-300">다른 시리즈 더 보기</p>
+        </section>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+        <section className="max-w-[1200px] w-full px-4 flex flex-col gap-6">
+          <h2 className="text-4xl font-bold text-gray-100 tracking-tight">
+            최근에 작성된 글
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2">
+            {Array(5)
+              .fill(null)
+              .map((_, index) => (
+                <PostCard
+                  key={index}
+                  title={"리액트 디버깅 팁 5가지 "}
+                  tags={["REACT", "JAVASCRIPT", "TYPESCRIPT"]}
+                  description={"리액트 환경에서 디버깅하는 팁을 소개합니다."}
+                />
+              ))}
+          </div>
+          <p className="text-gray-300">Read all posts</p>
+        </section>
+
+        <section className="max-w-[1200px] w-full px-4 flex flex-col gap-6">
+          <h2 className="text-4xl font-bold text-gray-100 tracking-tight">
+            웹 개발 주제
+          </h2>
+
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {["React", "Typescript", "Frontend"].map((tag) => (
+              <div key={tag} className="flex flex-col gap-4">
+                <div className="flex flex-row justify-between px-4 items-center">
+                  <p className="text-2xl font-semibold text-gray-200">{tag}</p>
+                  <p className="text-gray-300">더보기</p>
+                </div>
+
+                <div className="flex flex-col gap-4">
+                  {Array(3)
+                    .fill(null)
+                    .map((_, index) => (
+                      <PostCard
+                        key={index}
+                        title={"리액트 디버깅 팁 5가지 "}
+                        tags={["REACT", "JAVASCRIPT", "TYPESCRIPT"]}
+                        description={
+                          "리액트 환경에서 디버깅하는 팁을 소개합니다."
+                        }
+                      />
+                    ))}
+                </div>
+
+                <p className="text-gray-300">...총 N개의 포스트 더보기</p>
+              </div>
+            ))}
+          </div>
+        </section>
+      </main>
+    </div>
   );
 }
