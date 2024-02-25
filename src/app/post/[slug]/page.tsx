4371371878c744480665d564a64836bea76c5de8
권신학 <cheatkey.dev@gmail.com>
2024-02-25 22:26:27 +0900

    conflict 해결

--- src/app/post/[slug]/page.tsx
+++ src/app/post/[slug]/page.tsx
@@ -2,75 +2,17 @@ import { POST_PATH } from "@/constants";
 import path from "path";
 import fs from "fs/promises";
 import { Suspense } from "react";
-<<<<<<< Updated upstream
 
 import { MDXRemoteProps, compileMDX } from "next-mdx-remote/rsc";
 import _ from "lodash-es";
 import "@/style/codeBlock.css";
-<<<<<<< Updated upstream
 import AuraImage from "../../../utils/mdxRenderer/components/AuraImage";
-=======
-import AuraImage from "./components/AuraImage";
-
-const components: MDXRemoteProps["components"] = {
-  h1: (props) => (
-    <h1
-      className="text-3xl leading-10 font-bold text-post-header mt-6 tracking-tight"
-      {...props}
-    />
-  ),
-  h2: (props) => (
-    <h2
-      className={`text-2xl leading-10 font-bold text-post-header mt-6 tracking-tight`}
-      {...props}
-    />
-  ),
-  h3: (props) => (
-    <h3
-      className={`text-lg leading-10 font-bold text-post-header mt-6 tracking-tight`}
-      {...props}
-    />
-  ),
-  p: (props) => (
-    <p
-      className={`text-base leading-8 font-normal text-post-paragraph tracking-normal my-2 break-keep`}
-      {...props}
-    />
-  ),
-  strong: (props) => (
-    <strong
-      className={`text-base leading-8 font-bold text-post-header tracking-normal`}
-      {...props}
-    />
-  ),
-  a: (props) => (
-    <a className={`font-medium text-sky-600 items-baseline`} {...props} />
-  ),
-  ol: (props) => <ol className={`pl-5 font-medium list-decimal`} {...props} />,
-  ul: (props) => <ul className={`pl-5 list-disc`} {...props} />,
-  li: (props) => (
-    <li
-      className={`text-base font-normal text-post-paragraph tracking-normal my-2 break-keep`}
-      {...props}
-    />
-  ),
-  AuraImage,
-};
-=======
-import { compileMDX } from "next-mdx-remote/rsc";
-import _ from "lodash-es";
-import "@/style/codeBlock.css";
->>>>>>> Stashed changes
 import { getAllFiles } from "@/utils/getAllFiles";
 import {
   MDXcomponents,
   getCompileOption,
 } from "@/utils/mdxRenderer/mdxRenderer";
-<<<<<<< Updated upstream
-=======
 import TagContainer from "@/components/TagContainer";
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 
 export const generateStaticParams = async () => {
   const files = await getAllFiles(POST_PATH);
@@ -115,43 +57,6 @@ const PostPage = async ({ params }: PostPageProps) => {
     "utf8"
   );
 
-<<<<<<< Updated upstream
-  const { content, frontmatter } = await compileMDX<{ title: string }>(
-    getCompileOption(markdown)
-  );
-=======
-<<<<<<< Updated upstream
-  const { content, frontmatter } = await compileMDX<{ title: string }>({
-    source: markdown,
-    options: {
-      parseFrontmatter: true,
-      mdxOptions: {
-        remarkPlugins: [remarkGfm],
-        rehypePlugins: [[rehypePrettyCode as any, codeHighlightOption]],
-      },
-    },
-    components,
-  });
-
-  console.log("content:", content);
-
-  console.log(frontmatter);
->>>>>>> Stashed changes
-
-  return (
-    <Suspense fallback={<>Loading...</>}>
-      <div className="w-full max-w-[762px] text-base text-gray-800 dark:text-gray-100">
-        <h1 className="text-black dark:text-white">
-          제목은 {frontmatter.title} 입니다.
-        </h1>
-
-<<<<<<< Updated upstream
-        <article className="">{content}</article>
-=======
-        <article className="w-full max-w-[762px] px-8 text-base text-gray-800 dark:text-gray-100">
-          {content}
-        </article>
-=======
   const { content, frontmatter } = await compileMDX<PostFrontmatter>(
     getCompileOption(markdown)
   );
@@ -173,8 +78,6 @@ const PostPage = async ({ params }: PostPageProps) => {
         </div>
 
         <article className="pt-4">{content}</article>
->>>>>>> Stashed changes
->>>>>>> Stashed changes
       </div>
     </Suspense>
   );
--- src/components/PostCard.tsx
+++ src/components/PostCard.tsx
@@ -1,14 +1,6 @@
-<<<<<<< Updated upstream
-import Link from "next/link";
-
-=======
-<<<<<<< Updated upstream
-=======
 import Link from "next/link";
 import TagContainer from "./TagContainer";
 
->>>>>>> Stashed changes
->>>>>>> Stashed changes
 interface PostCardProps {
   title: string;
   tags: string[];
@@ -29,32 +21,7 @@ const PostCard = ({ title, description, tags, href }: PostCardProps) => {
             {description}
           </p>
 
-<<<<<<< Updated upstream
-          <div className="flex flex-row gap-2">
-            {tags.map((tag) => (
-              <span
-                className="text-gray-400 group-hover:text-gray-300 bg-gray-800 group-hover:bg-gray-700 text-sm px-2 py-1 rounded-xl transition-colors"
-                key={tag}
-              >
-                {tag}
-              </span>
-            ))}
-          </div>
-=======
-<<<<<<< Updated upstream
-        <div className="flex flex-row gap-2">
-          {tags.map((tag) => (
-            <span
-              className="text-gray-400 group-hover:text-gray-300 bg-gray-800 group-hover:bg-gray-700 text-sm px-2 py-1 rounded-xl transition-colors"
-              key={tag}
-            >
-              {tag}
-            </span>
-          ))}
-=======
           <TagContainer tags={tags} />
->>>>>>> Stashed changes
->>>>>>> Stashed changes
         </div>
       </div>
     </Link>
